297748e891677b70a941d325a6b31c6cf5424957
Ben Wu <wubin28@gmail.com>
2013-12-18 19:04:22 +0800

    Created class WordHandler.

---
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/WordHandler.java
@@ -0,0 +1,11 @@
+package com.wubinben.kata.fizzbuzz;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-18
+ * Time: 下午7:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordHandler {
+}
