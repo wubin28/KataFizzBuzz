ced79bd118deff52250db43b77ac5f843556de35
Ben Wu <wubin28@gmail.com>
2013-12-18 14:23:39 +0800

    Created method FizzBuzzMaker.translate() and made the first test green.

--- fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzBuzzMaker.java
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzBuzzMaker.java
@@ -8,4 +8,7 @@ package com.wubinben.kata.fizzbuzz;
  * To change this template use File | Settings | File Templates.
  */
 public class FizzBuzzMaker {
+    public static String translate(int i) {
+        return "1";
+    }
 }
