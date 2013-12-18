03f949ed7bab9f171b68f4dc828fc81b64e4f206
Ben Wu <wubin28@gmail.com>
2013-12-18 19:05:37 +0800

    Created class CommonNumberHandler.

---
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/CommonNumberHandler.java
@@ -0,0 +1,14 @@
+package com.wubinben.kata.fizzbuzz;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-18
+ * Time: 下午7:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class CommonNumberHandler extends WordHandler {
+    public CommonNumberHandler(WordHandler wordHandler) {
+        super(wordHandler);
+    }
+}
