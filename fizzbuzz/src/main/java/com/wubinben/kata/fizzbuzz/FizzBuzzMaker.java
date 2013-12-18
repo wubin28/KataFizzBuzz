b3177397d472d0b21ac323740ae92628d4caaee3
Ben Wu <wubin28@gmail.com>
2013-12-18 14:34:26 +0800

    Made all tests green with bad code.

--- fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzBuzzMaker.java
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzBuzzMaker.java
@@ -1,14 +1,20 @@
 package com.wubinben.kata.fizzbuzz;
 
+import sun.plugin.dom.exception.InvalidStateException;
+
 /**
- * Created with IntelliJ IDEA.
  * User: Ben
  * Date: 13-12-18
  * Time: 下午2:20
- * To change this template use File | Settings | File Templates.
  */
 public class FizzBuzzMaker {
     public static String translate(int i) {
-        return "1";
+        if (i == 1) {
+            return "1";
+        }
+        if (i == 3) {
+            return "Fizz";
+        }
+        throw new InvalidStateException("the number to be translated " + i + " is invalid.");
     }
 }
