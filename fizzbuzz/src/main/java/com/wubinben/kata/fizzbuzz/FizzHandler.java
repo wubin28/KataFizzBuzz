424473fdf3cb5578f203af2347b93e906ef9fbaa
Ben Wu <wubin28@gmail.com>
2013-12-18 19:09:51 +0800

    Added class FizzHandler.

--- fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzHandler.java
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzHandler.java
@@ -4,11 +4,11 @@ package com.wubinben.kata.fizzbuzz;
  * Created with IntelliJ IDEA.
  * User: Ben
  * Date: 13-12-18
- * Time: 下午7:07
+ * Time: 下午7:08
  * To change this template use File | Settings | File Templates.
  */
-public class BuzzHandler extends WordHandler {
-    public BuzzHandler(WordHandler wordHandler) {
+public class FizzHandler extends WordHandler {
+    public FizzHandler(WordHandler wordHandler) {
         super(wordHandler);
     }
 }
