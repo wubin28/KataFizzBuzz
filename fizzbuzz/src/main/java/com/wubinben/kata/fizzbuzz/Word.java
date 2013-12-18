496a3ddf212936c3eef489a082fbe0b13a57ad52
Ben Wu <wubin28@gmail.com>
2013-12-18 19:45:22 +0800

    Made all tests green again.

--- fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/Word.java
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/Word.java
@@ -1,16 +1,18 @@
 package com.wubinben.kata.fizzbuzz;
 
 /**
- * Created with IntelliJ IDEA.
  * User: Ben
  * Date: 13-12-18
  * Time: 下午7:10
- * To change this template use File | Settings | File Templates.
  */
 public class Word {
     private String word;
 
     public Word(String word) {
         this.word = word;
+    }
+
+    public String say() {
+        return word;
     }
 }
