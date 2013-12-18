40fd362403a23825183291bb0b32f7342be4ee48
Ben Wu <wubin28@gmail.com>
2013-12-18 19:10:42 +0800

    Added class FizzBuzzHandler.

---
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzBuzzHandler.java
@@ -0,0 +1,14 @@
+package com.wubinben.kata.fizzbuzz;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-18
+ * Time: 下午7:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class FizzBuzzHandler extends WordHandler {
+    public FizzBuzzHandler(WordHandler wordHandler) {
+        super(wordHandler);
+    }
+}
