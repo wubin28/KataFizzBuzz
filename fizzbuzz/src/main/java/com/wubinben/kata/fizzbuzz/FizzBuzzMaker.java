fdb2a6f8552c40b523b00eef4bc52bb0ef2c4493
Ben Wu <wubin28@gmail.com>
2013-12-18 14:21:53 +0800

    Created class FizzBuzzMaker.

---
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzBuzzMaker.java
@@ -0,0 +1,11 @@
+package com.wubinben.kata.fizzbuzz;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ben
+ * Date: 13-12-18
+ * Time: 下午2:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class FizzBuzzMaker {
+}
