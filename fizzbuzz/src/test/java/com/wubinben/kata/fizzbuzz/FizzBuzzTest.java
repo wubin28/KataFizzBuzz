89524033efe6da2eab39751083d763b94ead2fd8
Ben Wu <wubin28@gmail.com>
2013-12-18 13:59:06 +0800

    Added @RunWith annotation before the test class.

--- fizzbuzz/src/test/java/com/wubinben/kata/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/wubinben/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,9 +1,13 @@
 package com.wubinben.kata.fizzbuzz;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 /**
  * User: Ben
  * Date: 13-12-18
  * Time: 下午1:30
  */
+@RunWith(JUnit4.class)
 public class FizzBuzzTest {
 }
