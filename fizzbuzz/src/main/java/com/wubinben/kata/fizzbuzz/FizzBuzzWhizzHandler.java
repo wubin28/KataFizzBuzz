a519bd2b6fd93f93855d2bcca8a540c97013f2ec
Ben Wu <wubin28@gmail.com>
2013-12-19 09:47:50 +0800

    Added class FizzBuzzWhizzHandler and made all tests green.

---
+++ fizzbuzz/src/main/java/com/wubinben/kata/fizzbuzz/FizzBuzzWhizzHandler.java
@@ -0,0 +1,20 @@
+package com.wubinben.kata.fizzbuzz;
+
+/**
+ * User: Ben
+ * Date: 13-12-19
+ * Time: 上午9:42
+ */
+public class FizzBuzzWhizzHandler extends WordHandler {
+    public FizzBuzzWhizzHandler(WordHandler wordHandler) {
+        super(wordHandler);
+    }
+
+    @Override
+    public Word handle(int number) {
+        if (number % 5 == 0 && (new Integer(number)).toString().contains("3") && number % 7 == 0) {
+            return new Word("FizzBuzzWhizz");
+        }
+        return wordHandler.handle(number);
+    }
+}
